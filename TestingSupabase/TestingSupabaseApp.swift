40162c206bb9c4f760a2d5ca9df9a07d68d2cdb7
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2024-10-29 14:22:48 +0700

    Initial Commit

---
+++ TestingSupabase/TestingSupabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingSupabaseApp.swift
+//  TestingSupabase
+//
+//  Created by Marsha Likorawung on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingSupabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
